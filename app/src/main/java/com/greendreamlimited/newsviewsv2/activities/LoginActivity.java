9d2519f0e333a9b4fa2f61c64455ef014734978a
Sabuj <sabujsanacse@gmail.com>
2018-12-27 18:23:36 +0600

    Login activity added

---
+++ app/src/main/java/com/greendreamlimited/newsviewsv2/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.greendreamlimited.newsviewsv2.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.greendreamlimited.newsviewsv2.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
