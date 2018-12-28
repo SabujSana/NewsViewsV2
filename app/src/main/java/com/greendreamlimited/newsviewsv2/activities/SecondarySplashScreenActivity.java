664b414d4ebfc6296c5839184f25692cb42e94ed
Sabuj <sabujsanacse@gmail.com>
2018-12-28 20:49:46 +0600

    Secondary Splash Screen updated

--- app/src/main/java/com/greendreamlimited/newsviewsv2/activities/SecondarySplashScreenActivity.java
+++ app/src/main/java/com/greendreamlimited/newsviewsv2/activities/SecondarySplashScreenActivity.java
@@ -1,14 +1,20 @@
 package com.greendreamlimited.newsviewsv2.activities;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.greendreamlimited.newsviewsv2.R;
 import com.greendreamlimited.newsviewsv2.utils.SharedPreferenceManager;
+import com.koushikdutta.ion.Ion;
 
 public class SecondarySplashScreenActivity extends AppCompatActivity {
     private SharedPreferenceManager preferenceManager;
+    private ImageView ivSplash;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,15 @@ public class SecondarySplashScreenActivity extends AppCompatActivity {
             finish();
         }
         setContentView(R.layout.activity_secondary_splash_screen);
+        ivSplash = findViewById(R.id.iv_splash_git);
+        Ion.with(ivSplash).load("http://supdogs.com/wp-content/themes/resto/assets/images/load.gif");
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SecondarySplashScreenActivity.this, LoginActivity.class));
+                finish();
+            }
+        }, 3000);
     }
 }
